529c23df84b9215472b3ea8909b94cb225897906
CODIUM <codium@CODIUMs-MacBook-Pro-2.local>
2017-10-27 13:56:26 +0700

    no message

--- area.py
+++ area.py
@@ -1,11 +1,11 @@
 from math import pi
 
 n = input('')
-if n == 'c':
+if n == 'c'.upper() or n == 'c'.lower():
     raduis = input('')
     a = (pi * int(raduis) ** 2)
     print('%.2f' % a)
-elif n == 't':
+elif n == 't'.upper() or n == 't'.lower():
     high = input('')
     le = input('')
     print(int(high) * int(le))
---
+++ newworld.py
@@ -0,0 +1,4 @@
+a = 'bestuurdersaansprakelijkheidsverzekering'
+print(a[0]) # ได้ b
+print(a[-1]) # ได้ g
+print(a[int(len(a)/4)])
